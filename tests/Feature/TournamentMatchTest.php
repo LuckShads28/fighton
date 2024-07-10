d0dc4e40487cec9eb940129c7feb1a115cb6b250
LuckShads28 <gunturguntur163@gmail.com>
2024-07-10 18:47:27 +0700

    add blank unit

---
+++ tests/Feature/TournamentMatchTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Team;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TournamentMatchTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_create_match(): void
+    {
+
+    }
+
+    public function test_update(): void
+    {
+
+    }
+
+    public function test_delete_match(): void
+    {
+
+    }
+}
